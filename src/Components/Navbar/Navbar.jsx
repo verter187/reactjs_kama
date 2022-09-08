3c5015565e5686189cb522f83d4ffd945030f0a3
verter187 <verter187@inbox.ru>
2022-09-08 17:44:46 +0300

    issue_20220908_3: added new menu components

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,13 +3,17 @@ import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.sass";
 
 function Navbar({ menuItems }) {
+  console.log(s.activeLink);
   return (
     <div className={s.navbar}>
       {menuItems.map((item) => {
         let name = Object.keys(item)[0];
         return (
           <div key={Math.random()} className={s.item}>
-            <NavLink to={`/${name}`}>
+            <NavLink
+              to={`/${name}`}
+              className={({ isActive }) => (isActive ? s.activeLink : "")}
+            >
               {name.charAt(0).toUpperCase() + name.slice(1)}
             </NavLink>
           </div>
